2529a987a06511a80ff3fa59853d33d81b2fe6bd
phuonglinh99vn <83125820+phuonglinh99vn@users.noreply.github.com>
2022-11-13 22:42:26 +1100

    Update SquareInt.java

--- SquareInt.java
+++ SquareInt.java
@@ -8,17 +8,7 @@ public class SquareInt {
 	}
 	
 	public static int squares(int a, int b) {
-//		int[] array = new int[b - a + 1];
-//		array[0] = a;
-//		for (int i = 1; i < array.length; i++) {
-//			array[i] = array[i-1] + 1;
-//		}
-//		for (int i = (int) Math.round(Math.sqrt(a)); i <= Math.round(Math.sqrt(b)); i++) {
-//			int c =  Arrays.binarySearch(array, i*i);
-//			if(c > 0)
-//				System.out.println(i*i);; 
-//		}
-//		return 0;
+		
 		int count = 0;
 		for (int i = (int) Math.round(Math.sqrt(a)); i <= Math.round(Math.sqrt(b)); i++) {
 			if (i*i >= a && i*i <= b) {
@@ -28,8 +18,5 @@ public class SquareInt {
 		}
 		return count;
 	}
-	
-	
-//		
-//	}
+
 }
